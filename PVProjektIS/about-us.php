21f426febc5896332975126a5d2f99fe94767fd2
PetarVrdoljak <73475134+PetarVrdoljak@users.noreply.github.com>
2021-12-28 20:05:33 +0100

    Add files via upload

---
+++ PVProjektIS/about-us.php
@@ -0,0 +1,20 @@
+<?php 
+	print '
+	<header>
+	<div class="hero-image"></div>
+	</header>
+		<h1>About Us</h1>
+		<h2>Alan Turing</h2>
+		<video width="50%" controls poster="Pictures/Alan Turing.jpg" >
+		  <source src="video/AlanTuringBio.mp4" type="video/mp4">
+		  Your browser does not support HTML5 video.
+		</video>
+		<p>Turing was born in Maida Vale, London.</p>
+		<p>Turing studied as an undergraduate from 1931 to 1934 at Kings College, Cambridge, where he was awarded first-class honours in mathematics.</p>
+		<p>During the Second World War, Turing was a leading participant in the breaking of German ciphers at Bletchley Park. The historian and wartime codebreaker Asa Briggs has said, "You needed exceptional talent, you needed genius at Bletchley and Turings was that genius."</p>
+		<p>From September 1938, Turing worked part-time with the Government Code and Cypher School (GC&CS), the British codebreaking organisation. He concentrated on cryptanalysis of the Enigma cipher machine used by Nazi Germany, together with Dilly Knox, a senior GC&CS codebreaker.</p>
+		<p>By using statistical techniques to optimise the trial of different possibilities in the code breaking process, Turing made an innovative contribution to the subject. He wrote two papers discussing mathematical approaches, titled The Applications of Probability to Cryptographyand Paper on Statistics of Repetitions,which were of such value to GC&CS and its successor GCHQ that they were not released to the UK National Archives until April 2012, shortly before the centenary of his birth.</p>
+		<p>Within weeks of arriving at Bletchley Park,Turing had specified an electromechanical machine called the bombe, which could break Enigma more effectively than the Polish bomba kryptologiczna, from which its name was derived.</p>
+		<p>Between 1945 and 1947, Turing lived in Hampton, London,while he worked on the design of the ACE (Automatic Computing Engine) at the National Physical Laboratory (NPL). He presented a paper on 19 February 1946, which was the first detailed design of a stored-program computer.Von Neumanns incomplete First Draft of a Report on the EDVAC had predated Turings paper, but it was much less detailed and, according to John R. Womersley, Superintendent of the NPL Mathematics Division, it "contains a number of ideas which are Dr. Turings own".</p>
+		<p>Video courtesy of <a href="https://www.youtube.com/watch?v=MidJR581irA" target="_blank">Biography</a></p>';
+?>
---
+++ PVProjektIS/home.php
@@ -0,0 +1,20 @@
+<?php 
+	print '
+<h1>Turing Machine</h1>
+		<figure>
+			<img src="Pictures\Alan Turing.jpg" alt="HTML5" title="HTML5">
+			<figcaption>Alan Mathison Turing OBE FRS (<time datetime="1912-06-23">born 23 June 1912</time>)was an English mathematician, computer scientist, logician, cryptanalyst, philosopher, and theoretical biologist.</figcaption>
+		</figure>
+		<p>Turing was highly influential in the development of theoretical computer science, providing a formalisation of the concepts of algorithm and computation with the Turing machine, which can be considered a model of a general-purpose computer.</p>
+		<p>Turing is widely considered to be the father of theoretical computer science and artificial intelligence.Despite these accomplishments, he was never fully recognised in his home country during his lifetime due to the prevalence of homophobia at the time and because much of his work was covered by the Official Secrets Act.</p>
+		<p>During the Second World War, Turing worked for the Government Code and Cypher School (GC&CS) at Bletchley Park, Britains codebreaking centre that produced Ultra intelligence.</p>
+		<p>Turing played a crucial role in cracking intercepted coded messages that enabled the Allies to defeat the Nazis in many crucial engagements, including the Battle of the Atlantic, and in so doing helped win the war.</p>
+		<p>Due to the problems of counterfactual history, it is hard to estimate the precise effect Ultra intelligence had on the war,but at the upper end it has been estimated that this work shortened the war in Europe by more than two years and saved over 14 million lives.</p>
+		<p>After the war, Turing worked at the National Physical Laboratory, where he designed the Automatic Computing Engine. The Automatic Computing Engine was one of the first designs for a stored-program computer. In 1948, Turing joined Max Newmans Computing Machine Laboratory, at the Victoria University of Manchester, where he heland became interested in mathematical biology. He wrote a paper on the chemical basis of morphogenesisand predicted oscillating chemical reactions such as the Belousov–Zhabotinsky reaction, first observed in the 1960s.</p>
+		<p>Turing was prosecuted in 1952 for homosexual acts; the Labouchere Amendment of 1885 had mandated that "gross indecency" was a criminal offence in the UK. He accepted chemical castration treatment, with DES, as an alternative to prison. Turing died in 1954, 16 days before his 42nd birthday, from cyanide poisoning. An inquest determined his death as a suicide, but it has been noted that the known evidence is also consistent with accidental poisoning. </p>
+		<p>In 2009, following an Internet campaign, British Prime Minister Gordon Brown made an official public apology on behalf of the British government for "the appalling way he was treated". Queen Elizabeth II granted Turing a posthumous pardon in 2013. The "Alan Turing law" is now an informal term for a 2017 law in the United Kingdom that retroactively pardoned men cautioned or convicted under historical legislation that outlawed homosexual acts.</p>
+		<p>Source: <a href="https://en.wikipedia.org/wiki/Alan_Turing" target="_blank">Wiki</a></p>
+		<p>Social media:<br>
+			<a href="https://twitter.com/turinguiltylove" target="_blank"><img src="file:///C:\Users\flyin\Desktop\twitter.svg" alt="Twitter" title="Twitter" style="width:24px; margin-top:0.4em"></a>
+		</p>';
+?>
